2cc1e291391847a18769914def4a9065cb6b8775
abhishekes <abhishek.es@gmail.com>
2013-03-18 06:56:02 -0500

    Data Node : Working

--- zk_client/src/zk_client.java
+++ zk_client/src/zk_client.java
@@ -1,4 +1,5 @@
 import org.apache.zookeeper.*;
+import org.apache.zookeeper.ZooDefs.Ids;
 import org.slf4j.*;
 
 public class zk_client implements Watcher{
@@ -8,17 +9,17 @@ public class zk_client implements Watcher{
             System.exit(2);
         }
         String hostPort = args[0];
-        String znode = "";
+        String znode = "/group5";
         //String filename = args[2];
         //String exec[] = new String[args.length - 3];
         //System.arraycopy(args, 3, exec, 0, exec.length);
         try {
-            byte[] tmp = new String("Test").getBytes();
+            byte[] tmp = new String("Test").getBytes("UTF-16");
         	ZooKeeper zk = new ZooKeeper(hostPort, 3000, new zk_client());
-            zk.create(znode, tmp, null, CreateMode.PERSISTENT);
+            zk.create(znode, tmp, Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
         	//zk.setData(znode, tmp , 0);   
             byte[] tmp1 = zk.getData(znode, false, null);
-            System.out.println(tmp1.toString());
+            System.out.println("Output:" + new String(tmp1,"UTF-16"));
         } catch (Exception e) {
             e.printStackTrace();
         }
