e3fda0f32f128cc2b7f7d7344a272434601c3305
abhishekes <abhishek.es@gmail.com>
2013-03-30 20:53:35 -0500

    Parallel

--- zkClient/src/zkClient.java
+++ zkClient/src/zkClient.java
@@ -1,8 +1,10 @@
 import java.io.IOException;
+import java.util.Arrays;
 
 import org.apache.zookeeper.*;
 import org.apache.zookeeper.ZooDefs.Ids;
 import org.slf4j.*;
+
 
 
 public class zkClient implements Watcher{
@@ -10,7 +12,7 @@ public class zkClient implements Watcher{
 	
 	public static String randKey() {
 		long randomKey;
-		randomKey = (long)(Math.random() * (zkClient.maxKey+1));
+		randomKey = (long)(Math.random() * (zkClient.maxKey) + 1);
 		return new Long(randomKey).toString();
 	}
 	
@@ -19,18 +21,24 @@ public class zkClient implements Watcher{
             System.err.println("USAGE: Executor hostPort  [args ...]");
             System.exit(2);
         }
+        int[] distribution = new int[(int)(maxKey/10000) + 1];  
+        System.out.println(distribution[10009]);
         String hostPort = args[0];
+        
         String znode = "/db1";
-        long key;
+        //long key;
         
         
         zkClient zkClientObj = new zkClient();
         ZooKeeper zk = null;
+        int reads = 0, writes = 0;
         byte[] tmp = null;
 		try {
-			zk = new ZooKeeper(hostPort, 3000, zkClientObj);
+			zk = new ZooKeeper(hostPort, 30000, zkClientObj);
 			tmp = new String("db1").getBytes("UTF-16");
-			zk.create(znode, tmp, Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+			if (zk.exists(znode, null) == null) {
+				zk.create(znode, tmp, Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+			}
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
@@ -49,18 +57,44 @@ public class zkClient implements Watcher{
         try {
         	
         	long startTime = System.currentTimeMillis();
+        	long localStartTime = 0;
+        	long totalReadTime = 0;
+        	long totalWriteTime = 0;
+        	
+        	long readMin = 0;
+        	long writeMin = 0;
+        	long readMax = 0;
+        	long writeMax = 0;
         	
         	System.out.println("Start Time : " + startTime);
         	
         	String randKey = null;
         	byte[] retData = null;
-        	for (int  i = 0; i < 100000; i++) {
+        	for (int  i = 0; i < 2500; i++) {
 
         		randKey = zkClient.randKey();
-        		retData = zk.getDataByKey(znode, randKey);
+        		distribution[(int)((Long.parseLong(randKey)/10000))] += 1;
         		
-        		if(i % 50 == 0) {      			
-        			System.out.println("Time : " + (System.currentTimeMillis() - startTime) + ", KeyValue: " + retData.toString());
+        		if (Long.parseLong(randKey) % 2 == 0) {
+        			localStartTime = System.currentTimeMillis();
+        			retData = zk.getDataByKey(znode, randKey);
+        			totalReadTime += (System.currentTimeMillis() - localStartTime);
+        			reads++;
+        		    
+        		    
+        			//zk.setData(znode, randKey.getBytes(), -1);
+        		}else {
+        			writes++;
+        			localStartTime = System.currentTimeMillis();
+        			zk.setData(znode, randKey.getBytes(), -1);
+        			totalWriteTime += (System.currentTimeMillis() - localStartTime);
+        		    
+        		    
+        			//retData = zk.getDataByKey(znode, randKey);
+        		}
+        		
+        		if(i % 100 == 0) {      			
+        			System.out.println(/*"Time : " + (System.currentTimeMillis() - startTime) + */ " Progress : " + (float) i/200+ " % " + " KeyValue:  " + new String(randKey) + " Reads : " +reads + " Writes : " + writes);
         		}
         		
         		//zk.setData(znode, tmp , 0);   
@@ -68,7 +102,8 @@ public class zkClient implements Watcher{
         		//System.out.println("Output:" + new String(tmp1,"UTF-16"));
         	}
         	System.out.println("Total Time : " + (System.currentTimeMillis() - startTime));
-        	
+        	System.out.println(Arrays.toString(distribution));
+        	System.out.println(" Reads : "+ reads+ " Total Read Time : "  + totalReadTime + "\nWrites : "+ writes +" Total Write Time : "  + totalWriteTime);
         } catch (Exception e) {
             e.printStackTrace();
         }
