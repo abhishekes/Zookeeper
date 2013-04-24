ac9f5489925d6612b4569398004a688d8224d730
abhishekes <abhishek.es@gmail.com>
2013-04-24 02:15:57 -0500

    Re enabling prints

--- Zookeeper/src/java/main/org/apache/zookeeper/server/DataTree.java
+++ Zookeeper/src/java/main/org/apache/zookeeper/server/DataTree.java
@@ -742,7 +742,7 @@ public class DataTree {
 			// Use the db in here....
 			key = Arrays.copyOfRange(data, 0,16);
 			value = Arrays.copyOfRange(data, 16, 116);
-			//System.out.println("********* Key :" + new String(key) + " Value : " + new String(value));
+			System.out.println("********* Key :" + new String(key) + " Value : " + new String(value));
 			db.put(key, value);
 		} finally {
 			// Make sure you close the db to shutdown the 
@@ -869,7 +869,7 @@ public class DataTree {
 			}
 			//db = factory.open(new File(filePath), options);
 			tempkey = db.get(key.getBytes());
-			//System.out.println("*********Read Key :" + new String(key.getBytes()) /*+ " Value : " + new String(tempkey)*/);
+			System.out.println("*********Read Key :" + new String(key.getBytes()) + " Value : " + new String(tempkey));
 			//db.close();
 			/*byte fileData[] = new byte[numOfRows * keyLength]; 
 			Integer chunk = (int) java.lang.Math.ceil(Long.parseLong(key) / (double) numOfRows) ;
